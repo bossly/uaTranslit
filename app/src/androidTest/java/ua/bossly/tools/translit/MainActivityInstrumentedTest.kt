c0af8bf42f2a3371a938d1d67760425c2f39f545
oleg <oleg.baidalka@gmail.com>
2021-11-11 19:33:52 +0200

    upgrade gradle version

--- app/src/androidTest/java/ua/bossly/tools/translit/MainActivityInstrumentedTest.kt
+++ app/src/androidTest/java/ua/bossly/tools/translit/MainActivityInstrumentedTest.kt
@@ -1,5 +1,7 @@
 package ua.bossly.tools.translit
 
+import android.os.Bundle
+import android.util.Log
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -7,9 +9,14 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import tools.fastlane.screengrab.Screengrab
+import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy
+import tools.fastlane.screengrab.locale.LocaleTestRule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -22,6 +29,17 @@ class MainActivityInstrumentedTest {
 
     @get:Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @get:Rule
+    val localeTestRule = LocaleTestRule()
+
+    @Before
+    fun before() {
+        Screengrab.setDefaultScreenshotStrategy(UiAutomatorScreenshotStrategy())
+        val extras: Bundle = InstrumentationRegistry.getArguments()
+        // https://docs.fastlane.tools/getting-started/android/screenshots/#advanced-screengrab
+        Log.d("extras", extras.toString())
+    }
 
     @Test
     fun mainScreen() {
@@ -36,5 +54,7 @@ class MainActivityInstrumentedTest {
 
         // check result text changed
         onView(withId(R.id.outputField)).check(matches(withText("Taras Bulba")))
+
+        Screengrab.screenshot("home")
     }
 }
